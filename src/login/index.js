66d49171cf5dbeb2e50b11e5a6e15ad9706f02e3
Romain TRUCHI <romain.truchi.06@gmail.com>
2020-11-15 18:16:39 +0100

    Post: It works!

--- src/login/index.js
+++ src/login/index.js
@@ -12,18 +12,53 @@ export const TWLogin = async () => {
   return await TWETCH.authenticate();
 };
 
-export const IMBLogin = () => {
-  IMB.swipe({
-    amount: "0.03",
-    currency: "USD",
-    to: "31439@moneybutton.com",
-    onPayment: async (pay) => {
-      console.log({ pay });
-      let post = await TWETCH.publish("twetch/post@0.0.1", {
-        bContent: "Hello?",
-      });
-      console.log({ post });
-    },
-    onError: (err) => console.warn(err),
+export const IMBLogin = async () => {
+  const action = "twetch/post@0.0.1";
+  const { abi, payees } = await TWETCH.build(action, {
+    bContent: "Hello!",
   });
+  const outputs = [
+    {
+      currency: "BSV",
+      amount: 0,
+      script: window.bsv.Script.buildSafeDataOut(abi.toArray()).toASM(),
+    },
+    {
+      to: "31439@moneybutton.com",
+      amount: "0.00001000",
+      currency: "BSV",
+    },
+    ...payees,
+  ];
+
+  const cryptoOperations = [
+    { name: "myAddress", method: "address", key: "identity" },
+    {
+      name: "mySignature",
+      method: "sign",
+      data: abi.contentHash(),
+      dataEncoding: "utf8",
+      key: "identity",
+      algorithm: "bitcoin-signed-message",
+    },
+  ];
+
+  console.log(outputs, cryptoOperations);
+  window.confirm("Pay?") &&
+    IMB.swipe({
+      cryptoOperations,
+      outputs,
+      onPayment: async (pay) => {
+        console.log({ pay });
+        window.confirm("Post?") &&
+          (async () => {
+            let post = await TWETCH.publishRequest({
+              signed_raw_tx: pay.rawtx,
+              action,
+            });
+            console.log({ post });
+          })();
+      },
+      onError: (err) => console.warn(err),
+    });
 };
