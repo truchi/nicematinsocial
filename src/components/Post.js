47d40ff884846f32ff76ccf268292d0fccd854a6
Romain TRUCHI <romain.truchi.06@gmail.com>
2020-11-15 15:37:42 +0100

    Prettier

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,20 +1,30 @@
-import styles from "./Post.module.css"
+import styles from "./Post.module.css";
 
-export default (props) => {
-  const { id, bContent: content, createdAt, numLikes, userByUserId: user, youLiked: liked } = props;
+const Post = (props) => {
+  const {
+    id,
+    bContent: content,
+    createdAt,
+    numLikes,
+    userByUserId: user,
+    youLiked: liked,
+  } = props;
   const { icon, name } = user;
 
   return (
     <div className={styles.post}>
       <div className={styles.user}>
-        <div className={styles.icon} style={{
-          backgroundImage: `url("${icon}")`
-        }}></div>
-        { name }
+        <div
+          className={styles.icon}
+          style={{
+            backgroundImage: `url("${icon}")`,
+          }}
+        ></div>
+        {name}
       </div>
-      <div className={styles.content}>
-        {content}
-      </div>
+      <div className={styles.content}>{content}</div>
     </div>
-  )
-}
+  );
+};
+
+export default Post;
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,14 +1,16 @@
-import Post from "./Post"
-import styles from "./PostList.module.css"
+import Post from "./Post";
+import styles from "./PostList.module.css";
 
-export default (props) => {
+const PostList = (props) => {
   const { posts } = props;
 
   return (
     <div>
-      { posts.map((post) => {
-        return <Post key={post.id} className={styles.post} {...post} />
-      }) }
+      {posts.map((post) => {
+        return <Post key={post.id} className={styles.post} {...post} />;
+      })}
     </div>
-  )
-}
+  );
+};
+
+export default PostList;
